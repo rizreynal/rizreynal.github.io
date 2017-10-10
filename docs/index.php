40f24a982acaa11df69e93eb8d6a127aeaa4b8bb
Rizki Reynaldi <reynaldirizki43@gmail.com>
2017-10-10 22:53:21 +0700

    After Changes once
    After Changes once 1o oktober pukul sepuluh malam

---
+++ docs/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First Chalange</title>
+	<link rel="stylesheet" href="../css/mycss.css">
+</head>
+<body>
+
+<section class="tag first">
+<!-- 	<img src="../img/tagOnce1.jpg" alt=""> -->
+	<p class="test">this site is intended for learning</p>
+	<div class="top-navigator">
+		<ul>
+			<li><a href="#">Who Am I</a></li>
+			<li><a href="#">Know Me</a></li>
+			<li><a href="#">studying my nature</a></li>
+			<li><a href="#">close to me</a></li>
+		</ul>
+		<div class="clean-both"></div>
+	</div>
+		<div class="clean-both"></div>
+
+<!-- <button style="border: none;color: white;padding: 14px 28px;font-size: 16px;cursor: pointer;" >VisitMe</button> -->
+	<div class="headbutton">
+		<button class="button-head">Visit Me</button>
+	</div>
+
+
+</section>
+
+<section class="tag second">
+	
+	<nav class="header-second">
+		<h2>GALERI RIZKI REYNALDI</h2>
+		<p>here is an example of some collection photos from me</p>
+	</nav>
+
+	<nav class="framepic-second">
+		<nav class="collectpic pic1">
+			
+		</nav>
+		<nav class="collectpic pic2">
+			
+		</nav>
+		<nav class="collectpic pic3">
+			
+		</nav>
+		<div class="clean-both"></div>
+		<nav class="collectpic pic4">
+			
+		</nav>
+
+	</nav>
+
+</section>
+
+
+
+<section class="tag third">
+	<div class="third-once">
+		<h2 style="padding-top: 55px;">
+			OUR STORY
+		</h2>
+		<h4 style="margin-top: 10px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua.</h4>
+
+		<div style="text-align: left; max-width: 75%;margin: auto;margin-top: 65px;">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+	<div class="third-second">
+			<input type="button" class="" value="button" style="margin-top: 115px">
+	</div>
+</section>
+
+
+<section class="tag fourth">
+	<div class="fourth-once">
+		<h2 style="padding-top: 125px">
+			GET IN TOUCH
+		</h2>
+
+		<h3>
+			we would love to hear from now 
+		</h3>
+	</div>
+	<div class="fourth-second" >
+		<ul>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+		</ul>
+		<div class="clean-both"></div>
+	</div>
+		<div class="clean-both"></div>
+
+	<div class="kotak">kolom komentar ...</div>
+	<div class="kotak" style="height: 25px; margin-top: 5px; padding: 0px;text-align: center;">send</div>
+
+</section>
+
+<section class="tag sixth">
+
+<!-- 	<div class="atap">
+	</div> -->
+	<video width="60%" height="auto" controls>
+		<source src="../video/EvolutionofCSS.mp4" type="video/mp4">
+		Your browser does not support the video tag
+	</video>
+<!-- 	<embed width="60%" height="auto" controls>
+		<source src="../musik/Dewa19-DuaSejoli.mp3" type="musik/mp3">
+		Your browser does not support the video tag
+	</embed> -->
+
+	<div>
+			<audio src="../musik/Dewa19-DuaSejoli.mp3" play="disable" controls="controls"></audio>
+	</div>
+	
+	<div>
+		<a href="file:///H:/new%20XAMPP/htdocs/challenge/docs/index.php" style="margin: 0px">Go TO Google</a>
+	</div>
+
+</section>
+
+
+<section class="tag fifth">
+<div class="super-tagfifth">
+	<div class="tagfifth vonce">
+	</div>
+	<div class="tagfifth vsecond">
+	</div>
+	<div class="tagfifth vthird">
+	</div>
+</div>
+</section>
+
+
+
+
+<footer>copyright &copy; Rizki Reynaldi</footer>
+
+</body>
+</html>
